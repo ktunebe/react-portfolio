697c649d29dd9f0f879585a46b3fade561e1b920
ktunebe <ktunebe@gmail.com>
2024-07-25 16:20:06 -0500

    got carousel working

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,33 +1,105 @@
 import projects from '../assets/projects.json'
+import { useState, useRef } from 'react'
 
-export default function Project({ title, description, url, imgSrc, index }) {
+export default function Project() {
+	const [index, setIndex] = useState(0)
+	const imageRefs = useRef([])
+	const [translatePrevIndex, setTranslatePrevIndex] = useState(null)
+	const [translateNextIndex, setTranslateNextIndex] = useState(null)
 
-  function findPrevIndex() {
-    return (index - 1 >= 0) ? (index - 1) : projects.length - 1
-  }
-  function findNextIndex() {
-    return (index + 1 <= projects.length - 1) ? (index + 1) : 0
-  }
+	function handlePrevClick(index) {
+		setTranslatePrevIndex(index)
+		setTimeout(() => {
+			index - 1 >= 0 ? setIndex(index - 1) : setIndex(projects.length - 1)
+			setTranslatePrevIndex(null)
+		}, 500)
+	}
+	function handleNextClick(index) {
+		setTranslateNextIndex(index)
+		setTimeout(() => {
+			index + 1 <= projects.length - 1 ? setIndex(index + 1) : setIndex(0)
+			setTranslateNextIndex(null)
+		}, 500)
+	}
 
-  const prevIndex = findPrevIndex()
-  const nextIndex = findNextIndex()
-  
-  return (
-    <div id={`project${index}`} className="carousel-item relative w-full">
-    <div>
-      <div>
-        <img
-          src={imgSrc}
-          alt={`Screenshot of ${title}`}
-          className="w-full border-2 border-primary p-2" />
-      </div>
-        <div className="flex justify-around my-4">
-          <a href={`#project${prevIndex}`} className="btn rounded-full border-2 border-white text-white">❮ Prev</a>
-          <a href={`#project${nextIndex}`} className="btn rounded-full border-2 border-white text-white">Next ❯</a>
-        </div>
-      <h3 className='text-3xl my-4'>{title}</h3>
-      <p>{description}</p>
-    </div>
-  </div>
-  )
+	return (
+		<>
+			<div className="relative p-4">
+        {/* Next Image */}
+				<img
+					src={
+            // Will display project + 1 image at rest, but will display project - 2 when Prev Btn is clicked while it wraps around to eventually display project - 1 at other image position
+						translatePrevIndex === index
+							? projects[(index - 2 + projects.length) % projects.length].imgSrc
+							: projects[(index + 1 + projects.length) % projects.length].imgSrc
+					}
+					alt={`Screenshot of ${projects[index].title}`}
+					className={`w-[80%] border-2 border-white rounded-3xl absolute top-10 -right-3 sm:-right-6 md:-right-12 
+            ${
+              // Wrap direction based on prev or next click
+							translatePrevIndex === index
+								? '-translate-x-[35%] sm:-translate-x-[40%] z-10 duration-500'
+								: translateNextIndex === index
+								? '-translate-x-[15%] translate-y-[5%] scale-[1.15] z-50 duration-500'
+								: ''
+						}`}
+				/>
+        {/* Previous Image */}
+				<img
+					src={
+            // Will display project - 1 image at rest, but will display project + 2 when Next Btn is clicked while it wraps around to eventually display project + 1 at other image position
+            translateNextIndex === index
+              ? projects[(index + 2 + projects.length) % projects.length].imgSrc
+              : projects[(index - 1 + projects.length) % projects.length].imgSrc}
+					alt={`Screenshot of ${projects[index].title}`}
+					className={`w-[80%] border-2 border-white rounded-3xl absolute top-10 -left-3 sm:-left-6 md:-left-12 
+            ${
+              // Wrap direction based on prev or next click
+							translatePrevIndex === index
+								? 'translate-x-[15%] translate-y-[5%] scale-[1.15] z-50 duration-500'
+								: translateNextIndex === index
+								? 'translate-x-[35%] sm:translate-x-[40%] z-10 duration-500'
+								: ''
+						} `}
+				/>
+        {/* Current Image */}
+				<img
+					src={projects[index].imgSrc}
+					alt={`Screenshot of ${projects[index].title}`}
+					className={`w-full border-2 border-white rounded-3xl relative top-4 sm:top-0 left-0 
+            ${
+              // Wrap direction based on prev or next click
+							translatePrevIndex === index
+								? 'translate-x-[15%] sm:translate-x-[10%] -translate-y-[5%] scale-[.85] sm:scale-[.85] z-20 duration-500'
+								: translateNextIndex === index
+								? '-translate-x-[15%] sm:-translate-x-[10%] -translate-y-[5%] scale-[.85] sm:scale-[.85] z-20 duration-500'
+								: ''
+						}`}
+				/>
+			</div>
+
+			<div className="flex gap-3 items-center relative justify-between my-4 max-[400px]:px-1 px-3 w-full h-32">
+        {/* Prev Btn */}
+				<button
+					onClick={() => handlePrevClick(index)}
+					className="btn max-[400px]:p-1 rounded-full border-2 border-white text-white">
+					❮ Prev
+				</button>
+        {/* Project Title */}
+				<div className="px-2">
+					<h3 className="text-xl sm:text-3xl lg:text-5xl my-4">{`${
+						projects[index].title
+					} (${index + 1}/${projects.length})`}</h3>
+				</div>
+        {/* Next Btn */}
+				<button
+					onClick={() => handleNextClick(index)}
+					className="btn max-[400px]:p-1 rounded-full border-2 border-white text-white">
+					Next ❯
+				</button>
+			</div>
+      {/* Project Description */}
+			<p className="sm:text-xl">{projects[index].description}</p>
+		</>
+	)
 }
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -3,23 +3,11 @@ import Project from '../components/Project'
 
 export default function PortfolioPage() {
   return (
-    <div className='container my-8 mx-auto text-center'>
-      <h2 className='text-5xl font-bold mb-8'>Projects</h2>
-      <div className="carousel w-3/4">
-      {
-        projects.map((project, index) => {
-          return (
-            <Project
-              title={project.title}
-              description={project.description}
-              url={project.url}
-              imgSrc={project.imgSrc}
-              index={index}
-              key={project.title} />
-          )
-        })
-      }
-      </div>
+    <div className='container my-8 mx-auto text-center w-[80%]'>
+      <h2 className='text-3xl sm:text-5xl font-bold mb-8'>Projects</h2>
+
+      <Project />
+
     </div>
   )
 }
